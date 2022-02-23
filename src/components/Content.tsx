f7e03418ebad3dc0403749b881d7cb7833ed75fb
Lucas Gomes <lucasgomesbr@gmail.com>
2022-02-23 17:17:19 -0300

    desafio concluído

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,38 @@
-export function Content() {
-  // Complete aqui
+import { Header } from "./Header";
+import { MovieCard } from "./MovieCard";
+
+type ContentProps = {
+  title: string;
+  movies: {
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: {
+      Source: string;
+      Value: string;
+    }[];
+    Runtime: string;
+  }[]
+}
+
+export function Content({title, movies}: ContentProps) {
+  return (
+    <div className="container">
+      <Header title={title} />
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+type HeaderProps = {
+  title: string
+}
+
+export function Header({title}: HeaderProps) {
+  return (
+    <header>
+      <span className="category">
+        Categoria:<span> {title}</span>
+      </span>
+    </header>
+  );
+}
